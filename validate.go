32e34d58327eae92110aee3d0da4e469bc90ccfa
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2024-12-19 23:28:37 +0100

    Use X-Forwarded headers to build redirect correctly

--- validate.go
+++ validate.go
@@ -4,18 +4,38 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func handleValidate(w http.ResponseWriter, r *http.Request) {
 	accessTokenCookie, err := r.Cookie(cookieName)
 	if err != nil {
+		scheme := r.Header.Get("X-Forwarded-Proto")
+		if len(scheme) == 0 {
+			http.Error(w, "Missing 'X-Forwarded-Proto' header", http.StatusBadRequest)
+			return
+		}
+
+		host := r.Header.Get("X-Forwarded-Host")
+		if len(host) == 0 {
+			http.Error(w, "Missing 'X-Forwarded-Host' header", http.StatusBadRequest)
+			return
+		}
+
 		redirectUri := r.Header.Get("X-Forwarded-Uri")
 		if len(redirectUri) == 0 {
 			http.Error(w, "Missing 'X-Forwarded-Uri' header", http.StatusBadRequest)
 			return
 		}
 
-		url := oauth2Config.AuthCodeURL(redirectUri)
+		b := new(strings.Builder)
+		b.WriteString(scheme)
+		b.WriteString("://")
+		b.WriteString(host)
+		b.WriteString(redirectUri)
+		state := b.String()
+
+		url := oauth2Config.AuthCodeURL(state)
 		http.Redirect(w, r, url, http.StatusFound)
 		return
 	}
